f0417534fc5dc0bfbe3b0e1dcc0058b9cab5730e
Malik <97180802+mjones97@users.noreply.github.com>
2023-12-22 12:57:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function toggleNav() {
+    var mobileMenu = document.querySelector('.mobile-menu')
     var navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
+    mobileMenu.style.display = 'block';
 }
 
 const wrapper = document.querySelector('.wrapper');
 const indicators = [...document.querySelectorAll('.indicators button')];
 
-let currentTestimonial = 0;
+let currentTestimonial = 0; // Default 0
 
 indicators.forEach((item, i) => {
     item.addEventListener('click', () => {
@@ -14,5 +16,5 @@ indicators.forEach((item, i) => {
         wrapper.style.marginLeft = `-${100 * i}%`;
         item.classList.add('active');
         currentTestimonial = i;
-    });
-});
+    })
+})
